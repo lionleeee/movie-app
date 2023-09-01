be9c120a52920e7425d7fdc54861d7d55837c4bb
lionleee <lee97hun3@gmail.com>
2023-09-01 16:42:03 +0900

    feat : router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 
 import './App.css';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './Router';
+import {  QueryClient, QueryClientProvider } from 'react-query';
 
-function App() {
+const App : FC = () => {
+
+  const [queryClient] = useState(
+    ()=>
+      new QueryClient ({
+        defaultOptions :{
+          queries :{
+            refetchOnWindowFocus : false,
+            retry :0
+          },
+        },
+      })
+  );
+
+
   return (
-    <div className="App">
-
-    </div>
+    <RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <Router/>
+          </BrowserRouter>
+        </QueryClientProvider>
+    </RecoilRoot>
   );
 }
 
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,3 +1,3 @@
-import HomeContainer from "./HOmeContainer";
+import HomeContainer from "./HomeContainer";
 
 export default HomeContainer;
