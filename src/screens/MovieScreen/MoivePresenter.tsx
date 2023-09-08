5c05643d26a106ce64d89655b12e37a632862fdd
lionleee <lee97hun3@gmail.com>
2023-09-08 13:26:34 +0900

    -

--- src/screens/MovieScreen/MoivePresenter.tsx
+++ src/screens/MovieScreen/MoivePresenter.tsx
@@ -1,13 +1,43 @@
+import Loader from "../../components/Loader/Loder";
+
+interface MoivePresenterProps {
+    nowPlaying: any[] | null;
+    upcoming: any[] | null;
+    popular: any[] | null;
+    topRated: any[] | null;
+    error: string | null;
+    loading: boolean;
+    popularInfinite: any[] | null;
+    nowPlayingInfinite: any[] | null;
+    upcomingInfinite: any[] | null;
+    topRatedInfinite: any[] | null;
+}
 
 
+const MoivePresenter: React.FC<MoivePresenterProps> = ({
+    nowPlaying,
+    upcoming,
+    popular,
+    topRated,
 
+    loading,
 
-const MoivePresenter : React.FC<any>=({
+    nowPlayingInfinite,
+    popularInfinite,
+    upcomingInfinite,
+    topRatedInfinite
+}) => {
 
-})=>{
-    return (
+    const { location : {pathname}} = window;
+
+    console.log(nowPlaying);
+
+    return loading ? (
+            <Loader/>
+        ) :
+        (
         <div>무비 앱임</div>
-    );
+        );
 }
 
 export default MoivePresenter;
--- src/screens/MovieScreen/MovieContainer.tsx
+++ src/screens/MovieScreen/MovieContainer.tsx
@@ -1,8 +1,73 @@
 import { Component } from "react";
 import MoivePresenter from "./MoivePresenter";
+import { moviesApi } from "../../api/movie";
 
 
-class MovieContainer extends Component<{}, any>{
+
+interface MovieContainerState {
+    nowPlaying: any[] | null;
+    upcoming: any[] | null;
+    popular: any[] | null;
+    topRated: any[] | null;
+    error: string | null;
+    loading: boolean;
+    popularInfinite: any[] | null;
+    nowPlayingInfinite: any[] | null;
+    upcomingInfinite: any[] | null;
+    topRatedInfinite: any[] | null;
+  }
+
+class MovieContainer extends Component<{}, MovieContainerState>{
+
+    state: MovieContainerState ={
+        nowPlaying : null,
+        upcoming : null,
+        popular : null,
+        topRated  :null,
+        error : null,
+        loading : true,
+        nowPlayingInfinite : null,
+        upcomingInfinite : null,
+        popularInfinite :null,
+        topRatedInfinite : null
+    };
+
+    async componentDidMount() {
+        try{
+            const { data : {results : nowPlaying}} = await moviesApi.nowPlaying();
+            const { data : {results : upcoming}} = await moviesApi.upcoming();
+            const { data : {results : popular}} = await moviesApi.popular();
+            const { data : {results : topRated}} = await moviesApi.topRated();
+            const { data : {results : popularInfinite}} = await moviesApi.popularInfinite();
+            const { data : {results : nowPlayingInfinite}} = await moviesApi.nowPlayingInfinite();
+            const { data : {results : upcomingInfinite}} = await moviesApi.upcomingInfinite();
+            const { data : {results : topRatedInfinite}} = await moviesApi.topRatedInfinite();
+
+            this.setState({
+                nowPlaying,
+                upcoming,
+                popular,
+                topRated,
+                popularInfinite,
+                nowPlayingInfinite,
+                upcomingInfinite,
+                topRatedInfinite
+            });
+
+            
+
+
+        }
+        catch{
+            this.setState({error : "영화 정보를 찾을 수 없습니다."});
+        }
+        finally{
+            this.setState({loading : false});
+        }
+    }
+
+
+
 
     render(){
         return <MoivePresenter { ...this.state} />
